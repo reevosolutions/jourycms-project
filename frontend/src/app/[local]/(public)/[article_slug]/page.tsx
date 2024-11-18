53854e5fec79790e82243b961866ec80e80254a0
reevosolutions <48970385+reevosolutions@users.noreply.github.com>
2024-11-18 12:47:33 +0100

    update

--- frontend/src/app/[local]/(public)/[article_slug]/page.tsx
+++ frontend/src/app/[local]/(public)/[article_slug]/page.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable unicorn/no-useless-undefined */
-import { publicRoutes } from "@/config";
-import { getArticleBySlug, listArticles } from "@/themes/miqat/data";
+import {publicRoutes} from "@/config";
+import {getArticleBySlug, listArticles} from "@/themes/miqat/data";
 import ArticlePage from "@/themes/miqat/pages/article/article.omrah";
 
 const ROUTE = publicRoutes.homepage;
@@ -9,42 +9,46 @@ export const revalidate = 60;
 export const dynamicParams = true;
 
 export async function generateStaticParams() {
-  const data = await listArticles({
-    count: 100,
-    fields: ['slug'],
-  })
-  return (data?.data || []).map((post) => ({
-    article_slug: String(post.slug),
-  }))
+  try {
+    const data = await listArticles({
+      count: 100,
+      fields: ["slug"],
+    });
+    return (data?.data || []).map(post => ({
+      article_slug: String(post.slug),
+    }));
+  } catch {
+    return [];
+  }
 }
 
 export type PageProps = {
-  params: Promise<{ article_slug: string }>
-}
-export default async function Page({ params }: PageProps) {
-
-  const { article_slug } = await params
+  params: Promise<{article_slug: string}>;
+};
+export default async function Page({params}: PageProps) {
+  const {article_slug} = await params;
   /* -------------------------------------------------------------------------- */
   /*                                    QUERY                                   */
   /* -------------------------------------------------------------------------- */
-  let data: Levelup.CMS.V1.Content.Api.Articles.GetOne.Response | undefined = undefined;
+  let data: Levelup.CMS.V1.Content.Api.Articles.GetOne.Response | undefined =
+    undefined;
   try {
     data = await getArticleBySlug(article_slug);
   } catch (error: any) {
-    // do nothing  
+    // do nothing
     data = {
       error: {
         message: error.message,
         name: error.name,
         status: error.status || 500,
-      }
-    }
+      },
+    };
   }
 
   /* -------------------------------------------------------------------------- */
   /*                                   RETURN                                   */
   /* -------------------------------------------------------------------------- */
-  return <ArticlePage {...{ route: ROUTE }} initialData={data} />;
+  return <ArticlePage {...{route: ROUTE}} initialData={data} />;
   // return <div className="d">
   //   {data?.title}
   //   <div className="d">{article_slug}</div>
