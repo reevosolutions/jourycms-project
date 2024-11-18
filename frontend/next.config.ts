4c33290da1ceb81e854fce5aff48719bdc903c16
reevosolutions <48970385+reevosolutions@users.noreply.github.com>
2024-11-19 00:04:24 +0100

    update

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,4 +1,14 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
+import withSerwistInit from "@serwist/next";
+
+const withSerwist = withSerwistInit({
+  // Note: This is only an example. If you use Pages Router,
+  // use something else that works, such as "service-worker/index.ts".
+  swSrc: "src/sw.ts",
+  swDest: "public/sw.js",
+  cacheOnNavigation: true,
+
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -40,24 +50,29 @@ const nextConfig: NextConfig = {
   },
 
   async headers() {
-    return process.env.NODE_ENV === 'production' ? [{
-      // This doesn't work for 'Cache-Control' key (works for others though):
-      source: '/_next/(.*)',
-      headers: [
-        {
-          key: 'Cache-Control',
-          // Instead of this value:
-          value: 'public, max-age=604800, s-maxage=604800, stale-while-revalidate=604800',
-          // Cache-Control response header is `public, max-age=60` in production
-          // and `public, max-age=0, must-revalidate` in development
-        },
-        {
-          key: 'Access-Control-Allow-Origin', value: '*'
-        }
-      ],
-    },
-    ] : [];
+    return process.env.NODE_ENV === "production"
+      ? [
+          {
+            // This doesn't work for 'Cache-Control' key (works for others though):
+            source: "/_next/(.*)",
+            headers: [
+              {
+                key: "Cache-Control",
+                // Instead of this value:
+                value:
+                  "public, max-age=604800, s-maxage=604800, stale-while-revalidate=604800",
+                // Cache-Control response header is `public, max-age=60` in production
+                // and `public, max-age=0, must-revalidate` in development
+              },
+              {
+                key: "Access-Control-Allow-Origin",
+                value: "*",
+              },
+            ],
+          },
+        ]
+      : [];
   },
 };
 
-export default nextConfig;
+export default withSerwist(nextConfig);
