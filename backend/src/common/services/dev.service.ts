e08a02efed7988cb2b9521daedf4f7413b8369d2
reevosolutions <48970385+reevosolutions@users.noreply.github.com>
2024-11-23 11:31:40 +0100

    update

--- backend/src/common/services/dev.service.ts
+++ backend/src/common/services/dev.service.ts
@@ -222,6 +222,7 @@ export default class DevService extends BaseService {
   public async seedArticleTypes() {
     const scenario = this.initScenario(this.logger, this.seed);
     try {
+      this.logger.error('Deleted all article types')
       await this.articleTypeModel.deleteMany({});
       for (const type of articleTypesSeedData.types) {
         const existing = await this.articleTypeModel.findOne({
