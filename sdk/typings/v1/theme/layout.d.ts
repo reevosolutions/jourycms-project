cc0fba955a14857cdd24202a6c7c76f64e7cd7d7
reevosolutions <48970385+reevosolutions@users.noreply.github.com>
2024-11-23 23:33:44 +0100

    update

--- sdk/typings/v1/theme/layout.d.ts
+++ sdk/typings/v1/theme/layout.d.ts
@@ -4,6 +4,12 @@ declare module JouryCMS {
   export namespace Theme {
     export type LayoutProps = {
       route: Levelup.CMS.V1.UI.Routes.RouteItem;
+      headerControls?: {
+        path?: string;
+        onClick?: ()=>void | PromiseLike<void>;
+        title: string;
+        icons?: Levelup.CMS.V1.UI.IconType
+      }[]
       children: React.ReactNode;
     }
   }
--- sdk/typings/v1/theme/page.d.ts
+++ sdk/typings/v1/theme/page.d.ts
@@ -4,6 +4,12 @@ declare module JouryCMS {
   export namespace Theme {
     export type PageProps = {
       route: Levelup.CMS.V1.UI.Routes.RouteItem;
+      headerControls?: {
+        path?: string;
+        onClick?: ()=>void | PromiseLike<void>;
+        title: string;
+        icons?: Levelup.CMS.V1.UI.IconType
+      }[]
     }
   }
 }
