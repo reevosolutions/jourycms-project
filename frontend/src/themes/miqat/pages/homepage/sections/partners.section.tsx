83df6ee133075efa64dd410f94ec73a1f04c3a44
reevosolutions <48970385+reevosolutions@users.noreply.github.com>
2025-01-18 22:26:44 +0100

    update

--- frontend/src/themes/miqat/pages/homepage/sections/partners.section.tsx
+++ frontend/src/themes/miqat/pages/homepage/sections/partners.section.tsx
@@ -23,39 +23,39 @@ const HomepagePartnersSection: React.FC<
     <section className="bg-gray-50/50">
       <div className="pb-16 text-center">
         <div className="py-16 pb-4 md:block">
-          <h2 className="mt-0 pb-16 text-center text-3xl sm:text-5xl md:text-6xl font-bold text-darkblue-900">
+          <h2 className="mt-0 pb-16 text-center text-3xl font-bold text-darkblue-900 sm:text-5xl md:text-6xl">
             شركاؤنا في النجاح
           </h2>
 
-          <div className="grid grid-cols-2 md:grid-cols-4 items-center content-around md:gap-8 max-w-3xl mx-auto">
+          <div className="mx-auto grid max-w-3xl grid-cols-2 content-around items-center md:grid-cols-4 md:gap-8">
             <Image
               src="/assets/miqat/images/partners/partner1.png"
               width={196}
               height={196}
               alt=""
-              className=" mx-auto"
+              className="mx-auto"
             />
             <Image
               src="/assets/miqat/images/partners/partner2.png"
               width={196}
               height={196}
               alt=""
-              className=" mx-auto"
+              className="mx-auto"
             />
-            <Image
+            {/* <Image
               src="/assets/miqat/images/partners/partner3.png"
               width={196}
               height={196}
               alt=""
-              className=" mx-auto"
+              className="mx-auto"
             />
             <Image
               src="/assets/miqat/images/partners/partner4.png"
               width={196}
               height={196}
               alt=""
-              className=" mx-auto"
-            />
+              className="mx-auto"
+            /> */}
           </div>
         </div>
       </div>
